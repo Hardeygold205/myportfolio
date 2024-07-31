c6b0b5e7886f12b95e66b3f1226ad7ae9b6e0ff0
Surajudeen Hadi Ademola <138166910+Hardeygold205@users.noreply.github.com>
2024-07-31 18:02:27 +0100

    restructured

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const JetBrains = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-JetBrains",
+});
 
 export const metadata = {
   title: "Hadi Ademola",
@@ -11,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={JetBrains.className}>
+        <Navbar />
+        {children}</body>
     </html>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function Navbar() {
+  return <header>Header</header>;
+}
+
+export default Navbar;
